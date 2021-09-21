83b933c618e466221db7a5ff6f424b2acb6fcada
PabloHR-13 <84847573+PabloHR-13@users.noreply.github.com>
2021-09-21 21:01:35 +0200

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+#agregar interfaz de las cosas que quiera abrir añadiendo una opcion de abrir todo
+
+print("hello")
+os.startfile("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
+os.popen("C:\/Users\/pablo\/AppData\/Local\/WhatsApp\/WhatsApp.exe")
+print("goodbye")
